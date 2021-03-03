bc0cf54be006fe7557f52592e72c1d4f86c034dc
vinhhoang <vinhthontay113@gmail.com>
2021-03-03 21:46:05 +0700

    test2

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("vinh","test 2")
         Log.d("vinh","test1")
     }
 }
